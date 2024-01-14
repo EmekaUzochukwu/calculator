12ebf22c9a221b47943dfe6b2910c953a597b7ef
EmekaUzochukwu <156546631+EmekaUzochukwu@users.noreply.github.com>
2024-01-14 13:16:45 +0000

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+let calculation = JSON.parse(localStorage.getItem('calculation')) || '';
+
+        displayCalculation();
+
+        function updateCalculation(value) {
+          calculation += value;
+          displayCalculation();
+          localStorage.setItem('calculation', JSON.stringify(calculation));
+        }
+
+        function displayCalculation() {
+          document.querySelector('.js-update-Calculation').innerHTML = calculation;
+        }
